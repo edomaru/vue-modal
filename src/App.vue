1e159c719f0babcc8c4006ee8cff0a2e4d304f55
Eding Muhamad <masaruedo@yahoo.com>
2023-06-30 20:56:55 +0700

    Create base modal component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { ref } from "vue";
+import Modal from './components/modal/Modal.vue';
+
+const showModal = ref(false)
 </script>
 
 <template>
-  <h1>Vue Modal</h1>
+  <button @click="showModal = true">Show Modal</button>
+  <Modal :show="showModal" @close="showModal = false">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora amet saepe natus ut magni corrupti placeat veritatis vitae magnam aliquid autem reprehenderit, aperiam id dolorem sint nisi totam, provident iure.</p>
+  </Modal>
 </template>
---
+++ src/components/modal/Modal.vue
@@ -0,0 +1,59 @@
+<script setup>
+defineProps({
+    show: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <div class="v-modal" :class="{ hidden: !show }">
+        <div class="v-modal-dialog">
+            <div class="v-modal-content">
+                <slot />
+                <button @click="$emit('close')" type="button">Close</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+*,
+::before,
+::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.v-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1;
+}
+
+.v-modal-dialog {
+  max-width: 500px;
+  margin: 2rem auto;
+}
+
+.v-modal-content {
+  position: relative;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  background-clip: padding-box;
+  border-radius: 0.3rem;
+  padding: 1rem;
+}
+
+.hidden {
+    display: none;
+}
+</style>
